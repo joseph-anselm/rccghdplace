92272d5e90106a847dd6c2ba0ea611d6f791ab91
Coded <drjulius@Coded-MacBook-Pro.local>
2024-05-28 15:02:07 +0100

    maintenance

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
+// next.config.js
+module.exports = {
     images: {
       domains: ['cdn.sanity.io'],
     },
-  }
-  
-  module.exports = nextConfig;
+  };
   
